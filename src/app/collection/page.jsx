80d11bd551df6f297375e69db75fb24d7e9450a3
hovhannisiaan <hovhannisiaan@gmail.com>
2024-06-11 21:52:34 +0400

    first commit

---
+++ src/app/collection/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "@/Components/Layout/Layout";
+// import "@/app/globals.scss";
+
+export default function Page() {
+    return (
+        <Layout>
+            <h1>
+                COLLECTION PAGE
+            </h1>
+        </Layout>
+    );
+};
